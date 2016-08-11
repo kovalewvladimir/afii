7f207eed6e0db23aa9253daa0723ac28dd57d6bb
Vladimir <kovalew.vladimir@gmail.com>
2016-08-11 16:48:06 +0300

    Изменил печать QRcode

--- html_template/js/print.js
+++ html_template/js/print.js
@@ -1,17 +1,44 @@
-$(document).ready(function(){
-  $('#qrcodeCanvas').qrcode({
+// Обновить QRcode
+function updateImgQrcode(size, img){
+  var qrcodeDiv = document.createElement('div');
+  $(qrcodeDiv).qrcode({
     text   : location.href,
-    width  : 128,
-    height : 128
+    width  : size,
+    height : size
   });
-  var canvas = document.querySelector('#qrcodeCanvas canvas');
-  $('#qrcodeImg').attr("src", canvas.toDataURL());
+  var qrcodeCanvas = qrcodeDiv.children[0];
+  $(img).attr('src', qrcodeCanvas.toDataURL());
+}
 
+$(document).ready(function(){
+  
+  updateImgQrcode(128, '#qrcodeImg');
+
+  var page = $('#page');
+  var print = $('#print');
+  
+  // onChange изменение ширины страницы
+  $('#widthPage').change(function (){
+    page.css('width', this.value + 'mm');
+  });
+  
+  // onChange изменение высоты страницы
+  $('#heightPage').change(function (){
+    page.css('height', this.value + 'mm');
+  });
+  
+  // onChange изменение размера QRcode
+  $('#sizeQRcode').change(function (){
+    updateImgQrcode(this.value, '#qrcodeImg');
+  });
+  
+  // onClick вкл/выкл рамки QRcode
+  $('#isBorderQRcode').click(function (){
+      print.css('border', this.checked ? '0.5mm solid #000':'none');
+  });
+  
+  // onClick кнопка открытия модального окна "QR code"
   $('#btnQRcode').click(function (){
     $('#page').print();
   });
 });
-
-function updateImgQrcode(size){
-  
-}
