1aef9bc9236387bb397fbc77111941aa965d53cd
Vladimir <kovalew.vladimir@gmail.com>
2016-11-11 14:59:07 +0300

    Добавил переход на след страницу при авторизации

--- afii/loginsys/views.py
+++ afii/loginsys/views.py
@@ -10,17 +10,21 @@ from django.contrib import auth
 def login(request):
     args = {}
     if request.POST:
-        username = request.POST.get('username', '')
+        username = request.POST.get('username', '').lower()
         password = request.POST.get('password', '')
         user = auth.authenticate(username=username, password=password)
         if user is not None:
             auth.login(request, user)
-            return redirect('/')
+            next_page = request.COOKIES.get('auth_next_page', '/')
+            return redirect(next_page)
         else:
             args['login_error'] = 'Пользователь не найден'
             return render(request, 'loginsys/login.html', args)
     else:
-        return render(request, 'loginsys/login.html', args)
+        next_page = request.GET.get('next', '/')
+        response = render(request, 'loginsys/login.html', args)
+        response.set_cookie('auth_next_page', next_page)
+        return response
 
 
 def logout(request):
